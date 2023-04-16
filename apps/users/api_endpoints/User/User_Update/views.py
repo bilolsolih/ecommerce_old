78b23f07a50b530f9b42c85593e5b7c1a7271688
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-04-16 14:11:43 +0500

    User update view fixed

--- apps/users/api_endpoints/User/User_Update/views.py
+++ apps/users/api_endpoints/User/User_Update/views.py
@@ -1,14 +1,14 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import UpdateAPIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 
 from apps.common.permissions import IsTheSameUser
-from .serializers import UserUpdateSerializer
 from apps.users.models import User
+from .serializers import UserUpdateSerializer
 
 
-class UserUpdateAPIView(CreateAPIView):
+class UserUpdateAPIView(UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserUpdateSerializer
     permission_classes = [IsTheSameUser]
