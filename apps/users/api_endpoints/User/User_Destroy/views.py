a937cdb206becc4c885921490d90117cd3f08aeb
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-04-21 16:55:05 +0500

    Added price property to the CartEntry model

---
+++ apps/users/api_endpoints/User/User_Destroy/views.py
@@ -0,0 +1,12 @@
+from rest_framework.generics import DestroyAPIView
+
+from apps.common.permissions import IsTheSameUser
+from apps.users.models import User
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    permission_classes = [IsTheSameUser]
+    queryset = User.objects.all()
+
+
+__all__ = ['UserDestroyAPIView']
