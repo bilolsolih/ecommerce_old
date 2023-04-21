3c6d4615ac84a198e9afa2e72b869f996f9cda7c
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-04-21 08:53:46 +0500

    Added price property to the CartEntry model

--- apps/orders/api_endpoints/CartItem/CartItem_Create/views.py
+++ apps/orders/api_endpoints/CartItem/CartItem_Create/views.py
@@ -10,7 +10,25 @@ from .serializers import CartItemCreateSerializer
 
 
 class CartItemCreateAPIView(CreateAPIView):
-    pass
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemCreateSerializer
+    permission_classes = [IsAuthenticated]
+    parser_classes = [FormParser]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            vd = serializer.validated_data
+            cart = Cart.objects.get_or_create(user=request.user)
+            product = vd["the_product"]
+            delivery_service = vd["delivery_service"]
+            quantity = vd["quantity"]
+            entry = CartItem.objects.create(
+                cart=cart[0], product=product, delivery_service=delivery_service, quantity=quantity
+            )
+            entry.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 __all__ = ["CartItemCreateAPIView"]
